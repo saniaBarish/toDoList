4d00a8230b90c2b8bc1d290936550486d2cb3607
Aleksandt Barishpolets <sania93barish@gmail.com>
2018-11-23 16:03:26 +0200

    Updates

---
+++ precache-manifest.47c2779ff56adaa8db7e7a434ee2f957.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "6f3acc0de1aa0feb7c32ea8e335c47df",
+    "url": "/toDoList/static/media/x.6f3acc0d.svg"
+  },
+  {
+    "revision": "6603c14d6b98c47b0557",
+    "url": "/toDoList/static/js/runtime~main.6603c14d.js"
+  },
+  {
+    "revision": "3f5ee7a1371f3b2a51ba",
+    "url": "/toDoList/static/js/main.3f5ee7a1.chunk.js"
+  },
+  {
+    "revision": "b61ff21c33c2ff7d07a9",
+    "url": "/toDoList/static/js/1.b61ff21c.chunk.js"
+  },
+  {
+    "revision": "3f5ee7a1371f3b2a51ba",
+    "url": "/toDoList/static/css/main.6c2a8166.chunk.css"
+  },
+  {
+    "revision": "5a585566b597394f4b1dc9c2ad6b9883",
+    "url": "/toDoList/index.html"
+  }
+];
