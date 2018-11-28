befc379abc1a07ea896400300e4b2c709416c76c
Aleksandt Barishpolets <sania93barish@gmail.com>
2018-11-28 16:40:51 +0200

    Updates

---
+++ precache-manifest.00bd82052e631d0965c4e56abbce85d6.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "6f3acc0de1aa0feb7c32ea8e335c47df",
+    "url": "/toDoList/static/media/x.6f3acc0d.svg"
+  },
+  {
+    "revision": "c28fa661303d5ba7cc5a",
+    "url": "/toDoList/static/css/main.d1a8135b.chunk.css"
+  },
+  {
+    "revision": "14c1905c62793827a18a",
+    "url": "/toDoList/static/js/1.14c1905c.chunk.js"
+  },
+  {
+    "revision": "6603c14d6b98c47b0557",
+    "url": "/toDoList/static/js/runtime~main.6603c14d.js"
+  },
+  {
+    "revision": "65e117255d4a3580162e68d22b6120b8",
+    "url": "/toDoList/static/media/pen_alt_fill.65e11725.svg"
+  },
+  {
+    "revision": "c28fa661303d5ba7cc5a",
+    "url": "/toDoList/static/js/main.c28fa661.chunk.js"
+  },
+  {
+    "revision": "b840fd722fd618faa0374476f6e0d201",
+    "url": "/toDoList/static/media/pen_alt_fill_sm.b840fd72.svg"
+  },
+  {
+    "revision": "357e5df1902fe0cf762ce74b3049a569",
+    "url": "/toDoList/static/media/x_sm.357e5df1.svg"
+  },
+  {
+    "revision": "f6fc5b0dd22e2ae07d419de5fce72934",
+    "url": "/toDoList/static/media/pen_alt_stroke_sm.f6fc5b0d.svg"
+  },
+  {
+    "revision": "0d929bbe23954c74915ab3f0b97b8ec9",
+    "url": "/toDoList/static/media/pen_alt_stroke.0d929bbe.svg"
+  },
+  {
+    "revision": "57c4b74e677c5c84860c8d225918d45f",
+    "url": "/toDoList/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/toDoList/precache-manifest.cb75f6a3442cfd27750287d47e82bc0c.js"
+  "/toDoList/precache-manifest.00bd82052e631d0965c4e56abbce85d6.js"
 );
 
 workbox.clientsClaim();
